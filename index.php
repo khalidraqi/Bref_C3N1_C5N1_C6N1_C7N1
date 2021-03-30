6a9c925cee2afcc53b26620d1ccda4960dac5da0
khalid raqi <khaaliid.raaqii@gmail.com>
2021-03-30 09:53:36 +0100

    page web

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+
+  <h3>logo</h3>
+  <form action="">
+
+  <a class="btn btn-warning " href="login.php">Connexion</a>
+  <a class="btn btn-primary " href="register.php">Register</a>
+  </form>
+
+</nav>
+
+
+
+  
+</body>
+</html>
